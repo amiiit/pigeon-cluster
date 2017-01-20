b489f55b0ac5f4cb9a916012b0d1a540ecee1dc2
Amit Jakubowicz <jakubowicz.amit@gmail.com>
2017-01-20 22:29:15 +0100

    markers are positioned again

--- src/Cluster.js
+++ src/Cluster.js
@@ -5,68 +5,99 @@ const cloneElement = process.env.BABEL_ENV === 'inferno' ? Inferno.cloneVNode : 
 
 const [iLNG, iLAT] = [0, 1]
 
+const ensureArray = (children = []) => {
+    return Array.isArray(children) ? children : [children]
+}
 export default class Cluster extends Component {
 
-    render () {
-        const props = this.props
-        if (!Array.isArray(props.children)) {
-            return props.children
-        }
-        const {
-            mapState,
-            pixelToLatLng,
-            latLngToPixel,
-            children,
-            clusterMarkerRadius = 100,
-            maxZoom = 16
-        } = props
+    constructor (props) {
+        super(props)
+    }
 
-        const markers = children.map(function (marker) {
-            const pixel = latLngToPixel(marker.props.anchor)
-            return cloneElement(marker, {
-                pixelToLatLng,
-                latLngToPixel,
-                left: pixel[0],
-                top: pixel[1]
-            })
-        });
+    componentDidMount () {
+        this.rebuildData(this.props)
+    }
 
-        const pointsForClustering = markers.map((marker) => {
-            return {
-                vNode: marker,
-                geometry: {
-                    coordinates: marker.props.anchor
-                }
+    rebuildData (props) {
+        const pointsMap = this.generatePointsMap(props.children)
+        const index = this.loadPoints(pointsMap)
+        this.setState({
+            pointsMap,
+            index
+        })
+    }
+
+    generatePointsMap (children) {
+        const childrenArray = ensureArray(children)
+        const pointsMap = {}
+
+        childrenArray.forEach(child => {
+            const { key } = child
+            if (!key) {
+                throw new Error('Markers must have a key property', child)
             }
-        });
+            if (!child.props.anchor) {
+                throw new Error('Markers must have an anchor property', child)
+            }
+            pointsMap[key] = {
+                vNode: cloneElement(child, {
+                    latLngToPixel: this.props.latLngToPixel,
+                    pixelToLatLng: this.props.pixelToLatLng
+                }),
+                geometry: {
+                    coordinates: child.props.anchor
+                },
+                id: key
+            }
+        })
 
+        return pointsMap
+    }
+
+    loadPoints (pointsMap) {
         const index = supercluster({
-            radius: clusterMarkerRadius,
-            maxZoom
+            radius: this.props.clusterMarkerRadius || 100,
+            maxZoom: this.props.maxZoom || 16
         });
 
-        index.load(pointsForClustering);
+        index.load(Object.keys(pointsMap).map(id => pointsMap[id]));
+        return index
+    }
+
+    componentWillReceiveProps (nextProps) {
+        if (ensureArray(nextProps.children).length !== ensureArray(this.props.children).length) {
+            //cheap check for new children
+            this.rebuildData(nextProps)
+        }
+    }
+
+    render () {
+        const { latLngToPixel, mapState, className } = this.props
         const { ne, sw } = mapState.bounds
         const [westLng, southLat, eastLng, northLat] = [sw[iLNG], sw[iLAT], ne[iLNG], ne[iLAT]];
-        const markersAndClusters = index.getClusters([westLng, southLat, eastLng, northLat], Math.floor(mapState.zoom))
 
-        const displayElements = markersAndClusters.map(markerOrCluster => {
+        const markersAndClusters = this.state.index && this.state.index.getClusters([westLng, southLat, eastLng, northLat], Math.floor(mapState.zoom))
+
+        const displayElements = (markersAndClusters || []).map(markerOrCluster => {
             let displayElement
             const isCluster = markerOrCluster && markerOrCluster.properties && markerOrCluster.properties.cluster
+            const pixelOffset = latLngToPixel(markerOrCluster.geometry.coordinates)
             if (isCluster) {
-                const pixelOffset = latLngToPixel(markerOrCluster.geometry.coordinates)
                 const clusterElementKey = markerOrCluster.geometry.coordinates.toString()
                 displayElement = <DefaultClusterMarker key={clusterElementKey}
                                                        count={markerOrCluster.properties.point_count}
                                                        pixelOffset={pixelOffset}/>
             } else {
-                displayElement = markerOrCluster.vNode
+                displayElement = cloneElement(this.state.pointsMap[markerOrCluster.id].vNode, {
+                    left: pixelOffset[0],
+                    top: pixelOffset[1]
+                })
             }
             return displayElement
         })
 
         return (
-            <div className={props.className || ''}
+            <div className={className || ''}
                  style={{ position: 'absolute', height: mapState.height, width: mapState.width }}>
                 {displayElements}
             </div>
